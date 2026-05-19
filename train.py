0455d1da3bf17c6a86415c3ff6bf68068986ea5e
Valentin Bosch <val.bosch2001@gmail.com>
2026-05-19 19:27:00 +0200

    up

--- train.py
+++ train.py
@@ -24,6 +24,7 @@ def cleaning_csv(df_path, marker, encoder, element):
     df_label = pd.read_csv(df_path)
     df_label = df_label[df_label["stain"] == marker]
     df_label = df_label[["patient_id", element]].rename(columns={element: "Status"})
+    df_label = df_label.dropna(subset=["Status"])
     df_label["Status"] = df_label["Status"].astype(int)
     out_csv_marker = os.path.join("csv", f"{marker}_{encoder}.csv")
     df_label.to_csv(out_csv_marker, index=False)
