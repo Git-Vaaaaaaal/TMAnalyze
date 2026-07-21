2bb9d98246ce725305a34d419265ef995942295d
Valentin Bosch <val.bosch2001@gmail.com>
2026-07-21 16:46:03 +0200

    up calym

--- calym/extract_rois_from_svs.py
+++ calym/extract_rois_from_svs.py
@@ -38,7 +38,7 @@ from PIL import Image
 
 SVS_DIR      = r"\gold"       # dossier contenant les .svs
 GEOJSON_DIR  = r"\gold"   # dossier contenant les .geojson
-OUTPUT_DIR   = r"\data\TMAnalyze"    # dossier ou stocker les .tiff extraits
+OUTPUT_DIR   = r"\data\TMAnalyze8"    # dossier ou stocker les .tiff extraits
 
 MARGIN       = 0          # marge en pixels autour de chaque bbox
 TILE_SIZE    = 256        # taille des tuiles du TIFF de sortie
